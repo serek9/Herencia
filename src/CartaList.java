1439d44162397351b01c89da4bc5bcadc2b6548f
sergiodiaz <sergiodiaz808@gmail.com>
2016-11-30 13:53:53 +0100

    CartaList añadido.

---
+++ src/CartaList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class CartaList {
+
+    public ArrayList<Carta> lista = new ArrayList<>();
+
+    public CartaList() {}
+
+    public void alta(Carta c){
+        lista.add(c);
+    }
+
+
+}
